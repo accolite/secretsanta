d9be87efb1d40eced7f6e6c4f794256d40f36aad
Ankit Jain <ankit.jain@accoliteindia.com>
2017-12-19 16:59:01 +0530

    Fix for global page.

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -7,8 +7,8 @@
  * Handler for santa-child login
  */
 angular.module('secretSantaApp')
-  .controller('GlobalCtrl', ["$scope", "currentAuth",
-    function ($scope, currentAuth) {
+  .controller('GlobalCtrl', ["$scope", "currentAuth", "$rootScope", 
+    function ($scope, currentAuth, $rootScope) {
 
       $rootScope.$on('authData', function (event, data) {
         $scope.username = data.displayName || data.email;
