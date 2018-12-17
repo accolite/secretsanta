e4f7bdb11502a7b21a7286aae85d1918d4d91911
AravindM <aravindkumar.metku@github.com>
2018-12-17 18:11:20 +0530

    prod move 2018

--- SecretSantaNode/firebase_config.js
+++ SecretSantaNode/firebase_config.js
@@ -2,19 +2,18 @@
     // Initialize Firebase
     var firebase = require("firebase");
     var admin = require("firebase-admin");
-    var serviceAccount = require("./serviceAccountKey.json"); // chnage this for prod; https://firebase.google.com/docs/database/admin/start
-    // var serviceAccount = require("./serviceAccountKeyProd.json");
-    admin.initializeApp({
+    // var serviceAccount = require("./serviceAccountKey.json"); // chnage this for prod; https://firebase.google.com/docs/database/admin/start
+    var serviceAccount = require("./serviceAccountKeyProd.json");
+    /* admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
       databaseURL: "https://secretsanta-19ed6.firebaseio.com"
     });
-
+ */
     // change for prod
-    /* admin.initializeApp({
+    admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
       databaseURL: "https://secretsantaprod.firebaseio.com",
     });
- */
     // var config = {
     //     apiKey: "AIzaSyBMyZjbWf1J0-A677LvTRXQfb9t7a3y25o",
     //     authDomain: "secretsanta-accolite.firebaseapp.com",
